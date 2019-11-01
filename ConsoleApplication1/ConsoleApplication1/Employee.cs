9a0d242d08cf3c57f813dab87b7b0c766da17f86
QuangMinh187pm13970 <quangminhkt2000@gmail.com>
2019-11-01 11:39:19 +0700

    Employee

---
+++ ConsoleApplication1/ConsoleApplication1/Employee.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Employee
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private int salary;
+
+        public Employee(int id, string firstName, string lastName, int salary)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.salary = salary;
+        }
+        public int getID()
+        {
+            return id;
+        }
+        public string getFirstName()
+        {
+            return firstName;
+        }
+        public string getLastName()
+        {
+            return lastName;
+        }
+        public string getName()
+        {
+            return   firstName +" "+ lastName + " "; 
+        }
+        public int getSalary()
+        {
+            return salary;
+        }
+        public void setSalary(int newSalary)
+        {
+            this.salary = newSalary;
+        }
+        public int getAnnualSalary()
+        {
+            return this.salary * 12;
+        }
+        public int raiseSalary(int percent)
+        {
+            this.salary += percent;
+            return salary;
+        }
+        public string toString()
+        {
+            return string.Format("Employee[id ={0} , name = firstName lastName {1} , salary =  {2}",this.id,this.getName(),this.salary);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee(300,"nguyen","minh",1500000);
+            Console.WriteLine(e.toString());
+            e.setSalary(3000000);
+            Console.WriteLine("id " + e.getID() + " firstName " + e.getFirstName() + " lastName " + e.getLastName() + " salary " + e.getSalary());
+            Console.WriteLine(e.toString());
+            Console.WriteLine(e.getAnnualSalary());
+            Console.WriteLine(e.raiseSalary(10/100));
+        }
+    }
+}
